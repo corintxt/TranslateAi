fac07af0a5b30a13cb95270978903b44d0c3f2b6
Corin FAIFE <corin.faife@afp.com>
2025-01-06 11:05:03 -0500

    First shot at export>translate script

--- TranslateText.jsx
+++ TranslateText.jsx
@@ -1,10 +1,12 @@
+// Define text:
+var textToTranslate = "Verify the command file is receiving arguments.";
 // Write text to translate to a temp file:
-var textToTranslate = "Hello world";
 var tempFile = new File("/tmp/translate_args.txt");
 tempFile.open('w');
 tempFile.write(textToTranslate);
 tempFile.close();
-
-// Execute command file:
+// Execute command file from same directory:
+// File($.fileName) gets object reference to the current script,
+// then we get parent directory and append the command file name.
 var commandFile = File(File($.fileName).parent.fsName + '/translate.command');
-commandFile.execute();
+commandFile.execute()
