98cd4cb75b0ef20d81d910b818048553ef1113e0
Corin FAIFE <corin.faife@afp.com>
2025-02-28 10:04:30 -0500

    /Example folder in .gitignore 2

--- Examples/CountWords.jsx
+++ Examples/CountWords.jsx
@@ -4,7 +4,7 @@ ADOBE SYSTEMS INCORPORATED
 Copyright 2005-2010 Adobe Systems Incorporated 
 All Rights Reserved 
 
-NOTICE:  Adobe permits you to use, modify, and 
+NOTICE:  Adobes permits you to use, modify, and 
 distribute this file in accordance with the terms
 of the Adobe license agreement accompanying it.  
 If you have received this file from a source 
